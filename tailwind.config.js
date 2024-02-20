6104b5456b6cbb9c939661903766f1241ddd2c19
Mark Nash <abacefabmicroblast@hotmail.com>
2024-02-19 21:54:21 -0800

    Added prettier formatting by running "yarn lint" and ran it once except for Charels's partial changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#00a6df',
-        backdrop: '#f2f2f2',
-        almostblack: '#282828',
-        white: '#ffffff',
-        darkblue: '#00a6df33',
-        grayed: '#f2f2f2',
+        primary: "#00a6df",
+        backdrop: "#f2f2f2",
+        almostblack: "#282828",
+        white: "#ffffff",
+        darkblue: "#00a6df33",
+        grayed: "#f2f2f2",
         gray: {
-          900: '#202225',
-          800: '#2f3136',
-          700: '#36393f',
-          600: '#4f545c',
-          500: '#d4d7dc',
-          400: '#e3e5e8',
-          300: '#e6e6e6',
-          200: '#f2f3f5',
-        }
+          900: "#202225",
+          800: "#2f3136",
+          700: "#36393f",
+          600: "#4f545c",
+          500: "#d4d7dc",
+          400: "#e3e5e8",
+          300: "#e6e6e6",
+          200: "#f2f3f5",
+        },
       },
     },
   },
   plugins: [],
-}
-
+};
