786293ff728dbed5b589b7f6495c2ce78387655b
Mark Nash <abacefabmicroblast@hotmail.com>
2024-02-19 21:54:21 -0800

    Added prettier formatting every time `yarn start` is run

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './images/n64.jpg';
-import './App.css';
+import logo from "./images/n64.jpg";
+import "./App.css";
 
 function App() {
-
-  return (<div>
-    <h1 className='title text-3xl bg-blue-100 text-primary p-6 text-center'>weatherApp&reg;</h1>
-    <p className='p-6'> hello, maybe we put a field here for city name?</p>
-    <div className='p-6 flex justify-around font-semibold border rounded-xl shadow'>
-      <div>The weather today</div>
-      <div>60 degrees</div>
+  return (
+    <div>
+      <h1 className="title text-3xl bg-blue-100 text-primary p-6 text-center">
+        weatherApp&reg;
+      </h1>
+      <p className="p-6"> hello, maybe we put a field here for city name?</p>
+      <div className="p-6 flex justify-around font-semibold border rounded-xl shadow">
+        <div>The weather today</div>
+        <div>60 degrees</div>
+      </div>
     </div>
-  </div>
   );
 }
 
