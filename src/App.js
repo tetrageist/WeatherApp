b6f5c9c01b2f2dee453ba7b7d368bcf36d779051
tetrageist <zwesterman4985@gmail.com>
2024-02-19 20:25:41 -0800

    added name for app

--- src/App.js
+++ src/App.js
@@ -2,24 +2,9 @@ import logo from './images/n64.jpg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello! 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Yay Nintendo!
-        </a>
-      </header>
-    </div>
+  return (<div>
+    weatherApp
+  </div>
   );
 }
 
